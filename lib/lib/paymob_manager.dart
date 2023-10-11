2220dd44b2df17d21b2d19955d3e3295dbe5c9df
Ahmedzayan99 <122310086+Ahmedzayan99@users.noreply.github.com>
2023-10-11 21:03:16 +0300

    Update paymob_manager.dart

--- lib/lib/paymob_manager.dart
+++ lib/lib/paymob_manager.dart
@@ -98,3 +98,12 @@ class PaymobManager{
   }
 
 }
+/// Home Screen
+ Future<void> _pay() async{
+    PaymobManager().getPaymentKey(
+      10,"EGP"
+    ).then((String paymentKey) {
+      launchUrl(
+        Uri.parse("https://accept.paymob.com/api/acceptance/iframes/791787?payment_token=$paymentKey"),
+      );
+    });
